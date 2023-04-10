6f6485d51bbbeb2561c37f3955876bbc80f7668a
Jonathan G <jonathan79410@gmail.com>
2023-04-10 17:25:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from(process.env.DB_PASSWORD + '\0')
-  }
+  database: process.env.DB_NAME
 });
 
 
