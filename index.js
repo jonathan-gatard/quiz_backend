cf1daf3cbd220b6923d110f10b210dc5a11e4fcc
Jonathan G <jonathan79410@gmail.com>
2023-04-10 17:08:27 +0200

    add env

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
-
+require("dotenv").config();
 
 //MySQL connection
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'JonathanPassWorD',
-  database: 'training',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
