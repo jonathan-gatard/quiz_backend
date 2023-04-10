ea5c26c325aa2cbd88b14652534fd2a6f96e9d0b
Jonathan G <jonathan79410@gmail.com>
2023-04-10 16:49:18 +0200

    add env

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 
 //MySQL connection
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'JonathanPassWorD',
-  database: 'training',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
